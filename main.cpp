a9c25baf558395badd99cad4326ac299e3f95cbf
blue2222 <44754593+blue2222@users.noreply.github.com>
2018-11-04 11:49:08 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Hello.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <string>
+#include <iostream>
+
+int main()
+{
+    // Hello, world!
+    
+    std::cout << "Hello, world!";
+    
+    return 0;
+}
